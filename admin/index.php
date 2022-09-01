785adb3ceed26f718b2a15701c0bfe6e3e991a2f
Selvin Tomás <tomas.a12108@gmail.com>
2022-08-31 20:26:57 -0600

    Utilizando Active Record para listar las propiedades

--- admin/index.php
+++ admin/index.php
@@ -1,20 +1,11 @@
 <?php
-require '../includes/funciones.php';
-$auth = estaAutenticado();
-if(!$auth){
-    header('Location: /');
-}
+require '../includes/app.php';
+estaAutenticado();
 
+use App\Propiedad;
+// Implementar metodo para obtener todas las propiedades
 
-// Importar la conexion de la base de datos
-require '../includes/config/database.php';
-$db = conectarDB();
-
-// Escribir el query
-$query = "SELECT * FROM propiedades";
-
-// Consultar la base de datos
-$consulta = mysqli_query($db, $query);
+$propiedades = Propiedad::all();
 
 
 // Muestra mensaje condicional
@@ -78,22 +69,22 @@ incluirTemplates('header');
         </thead>
         <!-- Mostrar los resultados -->
         <tbody>
-            <?php while ($propiedad = mysqli_fetch_assoc($consulta)) : ?>
+            <?php foreach($propiedades as $propiedad ): ?>
                 <tr>
-                    <td><?php echo $propiedad['id']; ?></td>
-                    <td><?php echo $propiedad['titulo']; ?></td>
-                    <td><img class="imagen-tabla" src="/imagenes/<?php echo $propiedad['imagen']; ?>" alt=""></td>
-                    <td><?php echo "Q." . $propiedad['precio']; ?></td>
+                    <td><?php echo $propiedad->id; ?></td>
+                    <td><?php echo $propiedad->titulo; ?></td>
+                    <td><img class="imagen-tabla" src="/imagenes/<?php echo $propiedad->imagen; ?>" alt=""></td>
+                    <td><?php echo "Q." . $propiedad->precio; ?></td>
                     <td>
                         <form method="POST" class="w-100">
-                            <input type="hidden" name="id" value="<?php echo $propiedad['id']; ?>">
+                            <input type="hidden" name="id" value="<?php echo $propiedad->id; ?>">
                             <input type="submit" class="boton-rojo-block" value="Eliminar">
                         </form>
-                        <a href="/admin/propiedades/actualizar.php?id=<?php echo $propiedad['id']; ?>" class="boton-amarillo-block">Actualizar</a>
+                        <a href="/admin/propiedades/actualizar.php?id=<?php echo $propiedad->id; ?>" class="boton-amarillo-block">Actualizar</a>
                     </td>
 
                 </tr>
-            <?php endwhile; ?>
+            <?php endforeach; ?>
         </tbody>
     </table>
 </main>
